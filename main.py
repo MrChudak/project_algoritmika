6a768c9fd049d2e7a62ff1b7a642fcd5cc29a9ed
MrChudak <biker200721@mail.ru>
2023-07-12 19:43:02 +1000

    version 0.0.3

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 
 # Источник кода: https://dev-gang.ru/article/python-i-pyqt-sozdanie-menu-panelei-instrumentov-i-strok-sostojanija-l7ubf6mm7n/
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QMenuBar, QMenu, QToolBar, QAction
+from PyQt5.QtCore import Qt, QUrl
+# from PyQt5.QtWebKitWidgets import QWebView
+# from PyQt5.QtWebKit import QWebView
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QMenuBar, QMenu, QToolBar, QAction, QTextEdit, QDockWidget
 import sys
-
+import markdown
 
 
 
@@ -12,9 +15,9 @@ class Window(QMainWindow):
     def __init__(self, parent = None):
         super().__init__(parent)
         
-        self.setWindowTitle('Изменение менюшки')
+        self.setWindowTitle('School Helper')
         self.resize(700, 500)
-        self.central_Label = QLabel('hello world!')
+        self.central_Label = QLabel('Welcome!') #! central_Label
         self.central_Label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         self.setCentralWidget(self.central_Label)
 
@@ -23,6 +26,7 @@ class Window(QMainWindow):
         self._createMenuBar()
         self._createToolBars()
         self._connectActions()
+        self._init_web()
 
 
     def _createMenuBar(self):
@@ -42,58 +46,139 @@ class Window(QMainWindow):
 
 
         helpMenu = menuBar.addMenu('+')
-        helpMenu.addAction(self.helpcontent)
-        helpMenu.addAction(self.aboutAction)
+        helpMenu.addAction(self.graph_action)
+        helpMenu.addAction(self.txt_action)
         
 
         geometryMenu = helpMenu.addMenu('Фигура')
-        geometryMenu.addAction('3D фигура')
-        geometryMenu.addAction('2D фигура')
+        geometryMenu.addAction(self.two_d_figure_action) 
+        geometryMenu.addAction(self.three_d_figure_action)
 
 
     def _createToolBars(self):
+        pass
+        # # using Title
+        # fileToolBar = self.addToolBar('File')
 
-        # using Title
-        fileToolBar = self.addToolBar('File')
+        # # using a ToolBar object
+        # editToolBar = QToolBar('Edit', self)
+        # self.addToolBar(editToolBar)
 
-        # using a ToolBar object
-        editToolBar = QToolBar('Edit', self)
-        self.addToolBar(editToolBar)
-
-        # using a QToolBar object and a ToolBar Area
-        helpToolBar = QToolBar('Help', self)
-        self.addToolBar(Qt.LeftToolBarArea, helpToolBar)
+        # # using a QToolBar object and a ToolBar Area
+        # helpToolBar = QToolBar('Help', self)
+        # self.addToolBar(Qt.LeftToolBarArea, helpToolBar)
 
     
     def _createAction(self):
 
-        self.NewAction = QAction(self)
+        self.open_action = QAction(self)
+        self.open_action.setText('Open') # openAction
 
-        self.NewAction.setText('New')
-        self.openAction = QAction('open', self)
-        self.saveAction = QAction('save', self)
-        self.exitAction = QAction('exit', self)
-        self.copyAction = QAction('copy', self)
-        self.pasteAction = QAction('paste', self)
-        self.cutAction = QAction('cut', self)
-        self.helpcontent = QAction('График', self)
-        self.aboutAction = QAction('Текст', self)
+        self.close_action = QAction('Close', self) # closeAction
+        self.save_action = QAction('Save', self) # saveAction
+        self.exit_action = QAction('Exit', self) # exitAction
+        self.copy_action = QAction('Copy', self) # copyAction 
+        self.paste_action = QAction('Paste', self) # pasteAction 
+        self.cut_action = QAction('Cut', self) # cutAction 
+        self.two_d_figure_action = QAction('2D фигура', self)
+        self.three_d_figure_action = QAction('3D фигура', self)
+        self.graph_action = QAction('График', self) # graph_Action
+        self.txt_action = QAction('Текст', self) # aboutAction
         # self.geometryAction = QAction('Фигура', self)
 
 
+    def _init_web(self):
+        
+        self.web = QWebEngineView()
+        
 
 
 
 
-    def test_god(self):
+
+
+
+    def click_text(self):
         # menuBar = self.menuBar()
-        fileMenu = QMenu("File", self)
+        fileMenu = QMenu("Текст", self)
+        fileMenu.addAction(self.open_action)
+        fileMenu.addAction(self.save_action)
         # self.menuBar().removeMenu(helpMenu)
         self.menuBar().addMenu(fileMenu)
 
+    
+    def click_graph(self):
+        graficMenu = QMenu('График', self)
+        
+        self.menuBar().addMenu(graficMenu)
+    
+
+    def click_open(self):
+        self.central_Label.hide()
+        # self.left_Text_Edit = QTextEdit('Текст')
+        self.right_result = QTextEdit()
+        # self.right_result = QLabel("лол")
+        # self.left_Text_Edit.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+
+        # self.right_result.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+
+        # self.setCentralWidget(self.left_Text_Edit)
+        
+        self.setCentralWidget(self.right_result)
+        
+    
+    def click_save(self):
+        self.update()
+
+    
+    def click_2d_figure(self):
+        print('click...')
+
+    def click_3d_figure(self):
+        print('click...')
+
+
+
+
+
+    def update(self):
+        text = self.right_result.toPlainText()
+        with open("test.md", 'w', encoding = "UTF-8") as f_md:
+            f_md.write(text)
+        with open("test.md", 'r', encoding = "UTF-8") as f_md:
+            text_md = f_md.read()
+            text_html = markdown.markdown(text_md, extensions = ['extra'])
+            # print(text_html)
+        with open('test.html', 'w', encoding = 'UTF-8') as f_html:
+            f_html.write(text_html)
+        
+        
+        with open('test.html', 'r', encoding = 'UTF-8') as f_html:
+            text_html = f_html.read() 
+            self.web.setHtml(text_html)
+
+        self.right_result.hide()
+        self.setCentralWidget(self.web)
+        
+        
+
+
 
     def _connectActions(self):
-        self.aboutAction.triggered.connect(self.test_god)
+        self.txt_action.triggered.connect(self.click_text)
+        self.graph_action.triggered.connect(self.click_graph)
+        self.two_d_figure_action.triggered.connect(self.click_2d_figure)
+        self.three_d_figure_action.triggered.connect(self.click_3d_figure)
+        
+
+        # TODO: Тестовый сопособ под запись кнопки open и save внутри меню "текст" для проверки работы с .md и .html файлами
+        self.open_action.triggered.connect(self.click_open)
+        self.save_action.triggered.connect(self.click_save)
+        
+
+
+
+    
 
 
 
---
+++ test.md
@@ -0,0 +1,11 @@
+gfghfghfghfghfghfghfhfhfghfgh
+extensions
+# Cat
+
+## SuperCat
+
+
+list:
+1. omg
+2. gj
+3. nt
